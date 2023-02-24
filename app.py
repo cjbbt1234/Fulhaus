ee630af472816a0865e78025a388c18a00783dd4
Siyuan Shang <cjbbt12@gmail.com>
2023-02-23 21:22:24 -0500

    update

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ import torchvision.transforms as transforms
 from PIL import Image
 import numpy as np
 import io
+from torchvision.models import resnet18
+import torch.nn as nn
 
 # Load the model
-model = torch.load('model_ft.pth')
+# model = torch.load('model_ft.pth')
+model = resnet18(weights='ResNet18_Weights.DEFAULT')
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 3) 
+model.load_state_dict(torch.load('model_ft.pth'))
+model.eval()
 
 # define Flask app
 app = Flask(__name__)
@@ -28,11 +35,13 @@ def predict():
     image_tensor = preprocess(image)
     image_tensor = image_tensor.unsqueeze(0)
 
-    # model inference
-    outputs = model(image_tensor)
-    _, predicted = torch.max(outputs, 1)
-    labels = ['chair', 'sofa', 'bed']
-    result = labels[predicted[0].item()]
+    with torch.no_grad():
+        model.eval()
+        outputs = model(image_tensor)
+        _, predicted = torch.max(outputs, 1)
+        labels = ['chair', 'sofa', 'bed']
+        result = labels[predicted[0].item()]
+
 
     # return prediction result
     return jsonify({'result': result})
